f6bebb23130608f0fb251fe9037e0c991077e502
hyogeon <men1219@hanmail.net>
2020-02-18 21:50:52 +0900

    Fragment

--- app/src/main/java/com/example/android_basic_3/MainActivity.kt
+++ app/src/main/java/com/example/android_basic_3/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.example.android_basic_3
 
-import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -12,18 +10,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        webButton.setOnClickListener {
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://google.com/"))
-            startActivity(intent)
+        firstButton.setOnClickListener {
+            val transcation = supportFragmentManager.beginTransaction()
+            transcation.replace(R.id.contentFrame, FirstFragment())
+            transcation.addToBackStack(null)
+            transcation.commit()
         }
 
-        sendButton.setOnClickListener {
-            val intent = Intent(Intent.ACTION_SEND)
-            intent.type = "text/plain"
-            intent.putExtra(Intent.EXTRA_SUBJECT, "제목")
-            intent.putExtra(Intent.EXTRA_TEXT, "공유할 내용입니다. ")
-
-            startActivity(intent)
+        secondButton.setOnClickListener {
+            val transcation = supportFragmentManager.beginTransaction()
+            transcation.replace(R.id.contentFrame, SecondFragment())
+            transcation.addToBackStack(null)
+            transcation.commit()
         }
     }
 }
