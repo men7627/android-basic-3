0cb5612c9633a3b137d0183a66ec596bdd6ed660
hyogeon <men1219@hanmail.net>
2020-02-18 18:18:25 +0900

    TextView, EditText, TextWatcher

--- app/src/main/java/com/example/android_basic_3/MainActivity.kt
+++ app/src/main/java/com/example/android_basic_3/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.android_basic_3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        userInputText.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+                previewText.text = s
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+        })
     }
 }
