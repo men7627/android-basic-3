f817e69bcf8efad1d9126bcc3a38068eec5044fa
hyogeon <men1219@hanmail.net>
2020-02-18 20:11:16 +0900

    explicit intent, implicit intent

--- app/src/main/java/com/example/android_basic_3/MainActivity.kt
+++ app/src/main/java/com/example/android_basic_3/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.example.android_basic_3
 
-import android.annotation.SuppressLint
-import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,17 +12,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        moveButton.setOnClickListener {
-            val intent = Intent(applicationContext, SubActivity::class.java)
-            intent.putExtra("MESSAGE", "MainActivity에서 왔습니다. ")
-            startActivityForResult(intent, 1234)
+        webButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://google.com/"))
+            startActivity(intent)
         }
-    }
 
-    @SuppressLint("MissingSuperCall")
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if(requestCode == 1234 && resultCode == Activity.RESULT_OK){
-            messageText.text = data?.getStringExtra("MESSAGE")
+        sendButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/plain"
+            intent.putExtra(Intent.EXTRA_SUBJECT, "제목")
+            intent.putExtra(Intent.EXTRA_TEXT, "공유할 내용입니다. ")
+
+            startActivity(intent)
         }
     }
 }
